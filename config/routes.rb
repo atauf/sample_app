074985b7f0374f0404169b59d1ae922d5efb7bb3
Adam Taufmann <ataufmann@gmail.com>
2017-06-25 17:53:31 -1000

    Worked through 5.3.3 on Michael Hartl's Tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home'
+ # get 'static_pages/home'  #I guess this isn't needed if you have the root declared
 
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'  #changed from 'static_pages/help' format to make path and url shorter
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
